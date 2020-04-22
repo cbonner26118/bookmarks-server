d10b71da61d4576d3db160fb06fe43bebd952e10
Colton Bonner <cbonner26118@gmail.com>
2020-04-21 20:01:43 -0700

    Express server for the bookmarks client

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bookmarks-Server
+
+This will be a server for POST/GET/DELETE requests for a bookmark API.
---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+  {
+    id: 1,
+    title: 'Testing',
+    url: 'testurl',
+    rating: 5,
+    description: 'This is a test bookmark',
+  },
+];
+
+module.exports = { bookmarks };
