4b9137777c9ebf813f18e50a1b87c2ed1c76c5b5
Colton Bonner <cbonner26118@gmail.com>
2020-04-21 20:10:49 -0700

    Refactored API_TOKEN to be more apparent

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const helmet = require('helmet');
 const { v4: uuid } = require('uuid');
 const logger = require('./logger');
 const { bookmarks } = require('./store');
-const { NODE_ENV } = require('./config');
+const { NODE_ENV, API_TOKEN } = require('./config');
 
 const app = express();
 
@@ -18,10 +18,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+  if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
     logger.error(`Unauthorized request to path: ${req.path}`);
     return res.status(401).json({ error: 'Unauthorized request' });
   }
